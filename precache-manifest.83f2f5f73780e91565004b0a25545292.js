f809d76ff1b4e74fbce088b7973a52ff09c478dc
Yoni Moshe <42314457+yonimoshe@users.noreply.github.com>
2019-11-14 19:32:54 +0200

    Updates

---
+++ precache-manifest.83f2f5f73780e91565004b0a25545292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c6b692ff926415be610b2c30d9b702",
+    "url": "//yonimoshe.github.io/todo/index.html"
+  },
+  {
+    "revision": "5c71779807eaefe23ef8",
+    "url": "//yonimoshe.github.io/todo/static/css/main.794981d4.chunk.css"
+  },
+  {
+    "revision": "ea44b5a2e88f0d409692",
+    "url": "//yonimoshe.github.io/todo/static/js/2.6058d4cc.chunk.js"
+  },
+  {
+    "revision": "5c71779807eaefe23ef8",
+    "url": "//yonimoshe.github.io/todo/static/js/main.db52f460.chunk.js"
+  },
+  {
+    "revision": "4d467a1f4f364f4e708f",
+    "url": "//yonimoshe.github.io/todo/static/js/runtime-main.7e30d67e.js"
+  }
+]);
